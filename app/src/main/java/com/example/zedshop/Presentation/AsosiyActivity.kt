76c3cd4054b50d9abd97ebcc00aeb88b0b71244b
MAAB <orzubekjumaniyozov2010@gmail.com>
2025-05-23 16:44:51 +0500

    frtghyujki

---
+++ app/src/main/java/com/example/zedshop/Presentation/AsosiyActivity.kt
@@ -0,0 +1,18 @@
+package com.example.zedshop.Presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.zedshop.R
+
+class AsosiyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_asosiy)
+
+
+    }
+}
